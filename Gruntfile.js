34d1c2207bd92fa5bbf9249190ae83ed3d66f48e
Drew Miller <drew@onpatrol.co>
2013-10-17 12:04:17 +1100

    Remove mocha for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,26 @@
 module.exports = function(grunt) {
 
   // Add the grunt-mocha-test tasks.
-  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
-    // Configure a mochaTest task
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
-        },
-        src: ['test/**/*.js']
-      }
-    }
+watch: {
+  scripts: {
+    files: ['static/js/**/*.js', 'test/**/*.js'],
+    tasks: ['jshint'],
+    options: {
+      spawn: false,
+    },
+  },
+},
+jshint: {
+    all: ['Gruntfile.js', 'static/js/**/*.js', 'test/**/*.js']
+  }
+
   });
 
-  grunt.registerTask('default', 'mochaTest');
+  grunt.registerTask('default', ['jshint']);
 
 };
 
