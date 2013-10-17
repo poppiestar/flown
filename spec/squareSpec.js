20f02d56b5b13e4b2741f471609668bbd2f85c02
Drew Miller <drew@onpatrol.co>
2013-10-17 17:22:13 +1100

    Code and tests for creating and initialising a square object

--- spec/squareSpec.js
+++ spec/squareSpec.js
@@ -3,16 +3,47 @@ describe('Square', function() {
 
     it('should be an object', function() {
         expect(Flown.Square).toBeDefined();
+        expect(typeof Flown.Square).toBe('object')
     });
 
-    it('should have a create function', function() {
-        expect(Flown.Square.create).toBeDefined();
-        expect(typeof Flown.Square.create).toBe('function');
+    describe('creating a square', function() {
+
+        it('should have a create function', function() {
+            expect(Flown.Square.create).toBeDefined();
+            expect(typeof Flown.Square.create).toBe('function');
+        });        
+
+        it('should create a new Square object', function() {
+            var square = Flown.Square.create(5, 10);
+
+            expect(square).toBeDefined();
+        });
+
+        it('should initialise a square object upon creation', function() {
+            var square = Flown.Square.create(5, 10);
+
+            expect(square.x).toBe(5);
+            expect(square.y).toBe(10);
+        });
+
     });
 
-    it('should be able to initialise a new square', function() {
-        expect(Flown.Square.init).toBeDefined();
-        expect(typeof Flown.Square.init).toBe('function');
+    describe('initialising a square', function() {
+
+        it('should have an initialise function', function() {
+            expect(Flown.Square.init).toBeDefined();
+            expect(typeof Flown.Square.init).toBe('function');
+        });
+
+        it('should set the initial values of a new square', function() {
+            var square = Flown.Square.create(10, 20);
+
+            square.init(5, 10);
+
+            expect(square.x).toBe(5);
+            expect(square.y).toBe(10);
+        });
+
     });
 
 });
--- src/js/Square.js
+++ src/js/Square.js
@@ -1,10 +1,13 @@
 var Flown = this.Flown || {};
 
 Flown.Square = {
-    create: function() {
-
+    create: function(x, y) {
+        return Object.create(this).init(x,y);
     },
-    init: function() {
-        
+    init: function(x, y) {
+        this.x = x;
+        this.y = y;
+
+        return this;
     }
 };
