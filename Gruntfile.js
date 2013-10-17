af080560639ffb90028beae58d65b51dc48223f6
Drew Miller <drew@onpatrol.co>
2013-10-17 14:01:32 +1100

    Using jasmine for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,36 @@
 module.exports = function(grunt) {
 
   // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
-watch: {
-  scripts: {
-    files: ['static/js/**/*.js', 'test/**/*.js'],
-    tasks: ['jshint'],
-    options: {
-      spawn: false,
+    jasmine : {
+      src : 'src/**/*.js',
+      options : {
+        specs : 'spec/**/*.js'
+      }
     },
-  },
-},
-jshint: {
-    all: ['Gruntfile.js', 'static/js/**/*.js', 'test/**/*.js']
-  }
-
+    watch: {
+      scripts: {
+        files: ['static/js/**/*.js', 'test/**/*.js'],
+          tasks: ['jshint'],
+          options: {
+            spawn: false,
+          }
+        }
+    },
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        'test/**/*.js'
+      ]
+    }
   });
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'jasmine']);
 
 };
 
