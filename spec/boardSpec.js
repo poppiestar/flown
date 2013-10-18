2014b8796adb87bb6ffda0b508f33a808b6be957
Drew Miller <drew@onpatrol.co>
2013-10-19 09:56:53 +1100

    Added basic add and get functions and related tests for Board object

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -11,12 +11,74 @@ describe('Board', function() {
         it('should have a create function', function() {
             expect(Flown.Board.create).toBeDefined();
             expect(typeof Flown.Board.create).toBe('function');
-        });        
+        });
 
         it('should create a new Board object', function() {
+            var board = Flown.Board.create();
+
+            expect(board).toBeDefined();
+        });
+
+    });
+
+    describe('initialising a board', function() {
+
+        it('should have an init function', function() {
+            expect(Flown.Board.init).toBeDefined();
+            expect(typeof Flown.Board.init).toBe('function');
+        });
+
+        it('should initialise a Board object', function() {
+            var board = Flown.Board.create();
+
+            expect(board.getAll()).toEqual({});
+        });
+
+    });
+
+    describe('adding a path to the board', function() {
+
+        var board;
+
+        beforeEach(function() {
+            board = Flown.Board.create();
+        });
+
+        it('should have an add path function', function() {
+            expect(board.add).toBeDefined();
+            expect(typeof Flown.Board.add).toBe('function');
+        });
+
+        it('should add the path to the board', function() {
             var path = Flown.Path.create();
 
-            expect(path).toBeDefined();
+            path.add(Flown.Square.create(5, 10));
+            board.add('green', path);
+
+            expect(board.get('green')).toEqual(path);
+        });
+
+    });
+
+    describe('getting a path from the board', function() {
+
+        var board,
+            path;
+
+        beforeEach(function() {
+            board = Flown.Board.create();
+            path = Flown.Path.create();
+            path.add(Flown.Square.create(5, 10));
+            board.add('green', path);
+        });
+
+        it('should have a get path function', function() {
+            expect(board.get).toBeDefined();
+            expect(typeof Flown.Board.get).toBe('function');
+        });
+
+        it('should return the requested path', function() {
+            expect(board.get('green')).toEqual(path);
         });
 
     });
--- src/js/Board.js
+++ src/js/Board.js
@@ -3,7 +3,25 @@ var Flown = this.Flown || {};
 Flown.Board = {
 
     create: function() {
+        return Object.create(this).init();
+    },
 
+    init: function() {
+        this._paths = {};
+
+        return this;
+    },
+
+    add: function( name, path ) {
+        this._paths[name] = path;
+    },
+
+    get: function( name ) {
+        return this._paths[name];
+    },
+
+    getAll: function() {
+        return {};
     }
 
 };
