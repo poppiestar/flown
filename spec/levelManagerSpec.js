9ddb65677833ff352422378d288ea606ac756053
Drew Miller <drew@onpatrol.co>
2013-10-23 23:28:13 +1100

    Add levels to level manager and tests

--- spec/levelManagerSpec.js
+++ spec/levelManagerSpec.js
@@ -14,14 +14,21 @@ describe('Level Manager', function() {
         });
 
         it('creates a new level manager object', function() {
-            var levelManager = Flown.LevelManager.create();
+            var levelManager = Flown.LevelManager.create(exampleLevelPacks1);
 
             expect(levelManager).toBeDefined();
+            expect(levelManager._packs.length).toBe(exampleLevelPacks1.length);
         });
 
     });
 
     describe('initialising a level manager', function() {
+
+        var levelManager;
+
+        beforeEach(function() {
+            levelManager = Flown.LevelManager.create(exampleLevelPacks1);
+        });
 
         it('has an initialisation function', function() {
             expect(Flown.LevelManager.init).toBeDefined();
@@ -29,11 +36,62 @@ describe('Level Manager', function() {
         });
 
         it('sets the initial values of the level manager object', function() {
-            var levelManager = Flown.LevelManager.create();
+            levelManager.init(exampleLevelPacks2);
 
-            levelManager.init();
+            expect(levelManager._packs.length).toBe(exampleLevelPacks2.length);
+        });
+
+    });
+
+    describe('getting a level pack object from the level manager', function() {
+
+        var levelManager;
+
+        beforeEach(function() {
+            levelManager = Flown.LevelManager.create(exampleLevelPacks1);
+        });
+
+        it('has a getLevelPack function', function() {
+            expect(levelManager.getLevelPack).toBeDefined();
+            expect(typeof levelManager.getLevelPack).toBe('function');
+        });
+
+        it('returns a level set object', function() {
+            var levelPack = levelManager.getLevelPack(1);
+
+            expect(levelPack._name).toBe(exampleLevelPack1.name);
+            expect(levelPack._description).toBe(exampleLevelPack1.description);
+            expect(levelPack._sets.length).toBe(exampleLevelPack1.sets.length);
+        });
+
+    });
+
+    describe('adding a level pack to the level manager', function() {
+
+        var levelManager;
+
+        beforeEach(function() {
+            levelManager = Flown.LevelManager.create(exampleLevelPacks1);
+        });
+
+        it('has an addLevelPack function', function() {
+            expect(levelManager.addLevelPack).toBeDefined();
+            expect(typeof levelManager.addLevelPack).toBe('function');
+        });
+
+        it('adds a new level pack to the existing level packs', function() {
+            levelManager.addLevelPack(exampleLevelPack1);
+
+            expect(levelManager._packs.length).toBe(exampleLevelPacks1.length + 1);
         });
 
     });
 
 });
+
+
+/* 
+loadLevelPacks
+
+addLevelPack
+*/
--- src/js/LevelManager.js
+++ src/js/LevelManager.js
@@ -2,12 +2,28 @@ var Flown = this.Flown || {};
 
 Flown.LevelManager = {
 
-    create: function() {
-        return Object.create(this).init();
+    create: function( levelPacks ) {
+        return Object.create(this).init(levelPacks);
     },
 
-    init: function() {
+    init: function( levelPacks ) {
+        var pack;
+
+        this._packs = [];
+
+        for( pack in levelPacks ) {
+            this.addLevelPack(levelPacks[pack]);
+        }
+
         return this;
+    },
+
+    getLevelPack: function( levelPack ) {
+        return this._packs[levelPack-1];
+    },
+
+    addLevelPack: function( levelPack ) {
+        this._packs.push(Flown.LevelPack.create(levelPack));
     }
 
 };
