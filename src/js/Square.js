11ff714b0e8c1efbe003b013ec1063565b007e96
Drew Miller <drew@onpatrol.co>
2013-10-24 08:44:09 +1100

    Match formatting with other files

--- src/js/Square.js
+++ src/js/Square.js
@@ -1,16 +1,20 @@
 var Flown = this.Flown || {};
 
 Flown.Square = {
+
     create: function(x, y) {
         return Object.create(this).init(x,y);
     },
+
     init: function(x, y) {
         this.x = x;
         this.y = y;
 
         return this;
     },
+
     equals: function(square) {
         return this.x === square.x && this.y == square.y;
     }
+
 };
